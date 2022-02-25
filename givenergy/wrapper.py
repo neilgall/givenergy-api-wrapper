83b4d69c7f4783222d8e308fe44135edc1c50995
Neil Gall <neil@gall.scot>
2022-02-25 12:25:26 +0000

    Auto login when cookie expires

--- givenergy/wrapper.py
+++ givenergy/wrapper.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, session, abort
 import datetime
+import json
 import os
 import requests
 import secrets
@@ -14,18 +15,29 @@ app.secret_key = secrets.token_bytes(16)
 
 client = requests.Session()
 client.headers.update({'Accept': 'application/json'})
-client.post(f'{GIVENERGY_BASE_URL}/login?account={USERNAME}&password={PASSWORD}')
 
-plants = client.post(f'{GIVENERGY_BASE_URL}/plant/getPlantList').json()
-PLANT_ID = plants['rows'][0]['plantId']
+def login():
+  client.post(f'{GIVENERGY_BASE_URL}/login?account={USERNAME}&password={PASSWORD}')
+  plants = client.post(f'{GIVENERGY_BASE_URL}/plant/getPlantList').json()  
+  PLANT_ID = plants['rows'][0]['plantId']
+
+def post(path):
+  def do_post(path):
+    rsp = client.post(f'{GIVENERGY_BASE_URL}{path}')
+    if rsp.status_code != 200:
+      abort(500)
+    return rsp.json()
+
+  rsp_json = do_post(path)
+  if not rsp_json.get('success'):
+    login()
+    rsp_json = do_post(path)
+  return rsp_json
 
 
 @app.route('/solar/current')
 def get_plant_summary():
-  rsp = client.post(f'{GIVENERGY_BASE_URL}/plant/getPlantSummary')
-  if rsp.status_code != 200:
-    abort(500)
-  return rsp.content
+  return json.dumps(post('/plant/getPlantSummary'))
 
 
 @app.route('/solar/date')
@@ -34,7 +46,5 @@ def get_daily_data():
   month = int(request.args['month'])
   day = int(request.args['day'])
 
-  rsp = client.post(f'{GIVENERGY_BASE_URL}/plantChart/monthColumn?plantId={PLANT_ID}&year={year}&month={month}')
-  if rsp.status_code != 200:
-    abort(500)
-  return next(item for item in rsp.json()['data'] if item['day'] == day)
+  rsp = post(f'/plantChart/monthColumn?plantId={PLANT_ID}&year={year}&month={month}')
+  return next(item for item in rsp['data'] if item['day'] == day)

e7a20062c7ef938649f5d4ce1b8f2f48fe4540df
Neil Gall <neil@gall.scot>
2023-01-05 21:57:05 +0000

    Update to new API

--- givenergy/wrapper.py
+++ givenergy/wrapper.py
@@ -6,38 +6,27 @@ import requests
 import secrets
 
 
-GIVENERGY_BASE_URL = 'https://www.givenergy.cloud/GivManage/api'
-USERNAME = os.environ['USERNAME']
-PASSWORD = os.environ['PASSWORD']
+GIVENERGY_BASE_URL = 'https://api.givenergy.cloud/v1'
+API_TOKEN = os.environ['API_TOKEN']
 
 app = Flask(__name__)
 app.secret_key = secrets.token_bytes(16)
 
 client = requests.Session()
-client.headers.update({'Accept': 'application/json'})
+client.headers.update({
+  'Accept': 'application/json',
+  'Content-Type': 'application/json',
+  'Authorization': f'Bearer {API_TOKEN}'
+})
 
-def login():
-  client.post(f'{GIVENERGY_BASE_URL}/login?account={USERNAME}&password={PASSWORD}')
-  plants = client.post(f'{GIVENERGY_BASE_URL}/plant/getPlantList').json()  
-  PLANT_ID = plants['rows'][0]['plantId']
-
-def post(path):
-  def do_post(path):
-    rsp = client.post(f'{GIVENERGY_BASE_URL}{path}')
-    if rsp.status_code != 200:
-      abort(500)
-    return rsp.json()
-
-  rsp_json = do_post(path)
-  if not rsp_json.get('success'):
-    login()
-    rsp_json = do_post(path)
-  return rsp_json
+device_info = client.get(f'{GIVENERGY_BASE_URL}/communication-device').json()
+print(device_info)
+inverter = device_info['data'][0]['inverter']['serial']
 
 
 @app.route('/solar/current')
-def get_plant_summary():
-  return json.dumps(post('/plant/getPlantSummary'))
+def get_current_data():
+  return client.get(f'{GIVENERGY_BASE_URL}/inverter/{inverter}/system-data/latest').content
 
 
 @app.route('/solar/date')
